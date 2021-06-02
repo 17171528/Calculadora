f1ce2904bd1a9aaf098c4c250491a984a878ba52
AngelArturo13 <17171458@itculiacan.edu.mx>
2021-06-01 23:17:43 -0600

    Programar la funcion de division

--- app/src/main/java/wsCalculadora/App.java
+++ app/src/main/java/wsCalculadora/App.java
@@ -17,6 +17,7 @@ public class App {
         System.out.println("2+3="+ c.suma(2, 3));
         System.out.println("2*3=" + c.multiplica(2, 3));
         System.out.println("3.4 * 4.5=" + c.multiplica(3.4,4.5));
+        System.out.println("69 / 3=" + c.divide(69, 3));
 
         System.out.println("Sen(0)="+c.seno(0));
         System.out.println("Cos(0)="+c.coseno(0));
--- app/src/main/java/wsCalculadora/Calculadora.java
+++ app/src/main/java/wsCalculadora/Calculadora.java
@@ -1,13 +1,24 @@
 package wsCalculadora;
 
 public class Calculadora {
-    public double suma(double x, double y)
-    {
+    public double suma(double x, double y) {
         return x + y;
     }
 
     public double multiplica(double x, double y) {
         return x * y;
+    }
+
+    // Division
+    public double divide(double x, double y) {
+        double resultado = 0;
+        try {
+            resultado = x / y;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultado;
     }
 
     public double seno(double x) {
